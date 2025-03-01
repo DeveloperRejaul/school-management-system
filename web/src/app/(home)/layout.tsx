becdb1707cee360367e085d2fe011639018a9b1e
Rezaul Karim <rezaul@orangetoolz.com>
2025-03-01 18:54:44 +0600

    add

--- web/src/app/(home)/layout.tsx
+++ web/src/app/(home)/layout.tsx
@@ -1,21 +1,38 @@
+import Image from 'next/image';
 import React from 'react';
-
-export default function layout({ children }: Readonly<{children: React.ReactNode}>) {
+import Logo from '@/assets/logo.png'
+export default function layout() {
   return (
-    <div className='flex container flex-col'>
-      <nav className='flex w-full justify-between '>
-        <div>Logo</div>
-        <div className='flex space-x-10'>
-          <div className='cursor-pointer'>Home</div>
-          <div className='cursor-pointer'>Services</div>
-          <div className='cursor-pointer'>Proses</div>
-          <div className='cursor-pointer'>Pricing</div>
-          <div className='cursor-pointer'>About</div>
+    <div className='w-full'>
+      <section className='header flex flex-1 w-full justify-center flex-col'>
+        <div className='overly'/>
+        <nav className='fixed top-0 flex justify-center h-[80px] z-10 w-full'>
+          <div className='container flex justify-between items-center h-full'>
+            <Image src={Logo} height={100} width={100} alt='Logo'/>
+            <div className='flex space-x-10'>
+              <div className='btn'>Home</div>
+              <div className='btn'>Services</div>
+              <div className='btn'>Proses</div>
+              <div className='btn'>Pricing</div>
+              <div className='btn'>About</div>
+            </div>
+            <div className='flex gap-x-3'>
+              <div className='!bg-blue-700 btn'>Contact Us</div>
+            </div>
+          </div>
+        </nav>
+        <div className='flex h-[100vh] container m-auto'>
+          <div className='pt-[80px]'>
+            {/* contents */}
+            <h1>Your school partner</h1>
+          </div>
         </div>
-        <div>Contact Us</div>
-        <button>Change Them</button>
-      </nav>
-      {children}
+      </section>
+      <section style={{height: "100vh"}}>
+        <div className='flex flex-1 w-full justify-center'>
+          <div></div>
+        </div>
+      </section>
     </div>
   )
 }
